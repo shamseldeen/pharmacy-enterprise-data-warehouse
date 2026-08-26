bedd32544b4416a91a80d19e06c3fce5815702a2
shamseldeen ismaiil <pharmacian2050@gmail.com>
2026-08-26 08:47:24 +0300

    Update init_database.sql

--- sql/setup/init_database.sql
+++ sql/setup/init_database.sql
@@ -1,27 +1,71 @@
 /*
 ===============================================================================
 Create Database and Schemas
-Project: Pharmacy Enterprise Data Warehouse
+===============================================================================
+Script Purpose:
+    This script creates a new database named 'PharmacyEnterpriseDW' after
+    checking if it already exists.
+
+    If the database exists, it is dropped and recreated.
+
+    Additionally, the script creates the three schemas used in the
+    Medallion Architecture:
+
+        - bronze
+        - silver
+        - gold
+
+WARNING:
+    Running this script will drop the entire 'PharmacyEnterpriseDW'
+    database if it already exists.
+
+    All data inside the database will be permanently deleted.
+
+    Proceed with caution and make sure you have proper backups before
+    running this script in any environment containing important data.
 ===============================================================================
 */
 
--- Create Database
 USE master;
 GO
 
+
+-- Drop and recreate the 'PharmacyEnterpriseDW' database
+IF EXISTS (
+    SELECT 1
+    FROM sys.databases
+    WHERE name = 'PharmacyEnterpriseDW'
+)
+BEGIN
+    ALTER DATABASE PharmacyEnterpriseDW
+        SET SINGLE_USER
+        WITH ROLLBACK IMMEDIATE;
+
+    DROP DATABASE PharmacyEnterpriseDW;
+END;
+GO
+
+
+-- Create the 'PharmacyEnterpriseDW' database
 CREATE DATABASE PharmacyEnterpriseDW;
 GO
 
--- Use Database
+
+-- Use the newly created database
 USE PharmacyEnterpriseDW;
 GO
 
--- Create Schemas
+
+-- Create Bronze Schema
 CREATE SCHEMA bronze;
 GO
 
+
+-- Create Silver Schema
 CREATE SCHEMA silver;
 GO
 
+
+-- Create Gold Schema
 CREATE SCHEMA gold;
 GO
